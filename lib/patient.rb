1257d18add9b31d226349a6adf15279750510def
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-06 22:48:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,11 +2,16 @@ class Patient
   
   attr_accessor :name, :doctor, :appointment
   
-
+  @@all = []
+  
  def initialize(name)
    @name = name
    @@all << self 
  end
+ 
+ def self.all 
+   @@all 
+ end 
  
  def new_appointment(doctor, date)
    Appointment.new(doctor, self, date)
